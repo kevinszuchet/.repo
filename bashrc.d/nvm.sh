6acf3b5b4b93da5a0dc1c6cb312b47817d789b5e
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2020-02-10 15:34:06 -0300

    Create nvm.sh

---
+++ bashrc.d/nvm.sh
@@ -0,0 +1,6 @@
+# nvm for handling node versions
+if which thefuck >/dev/null 2>&1 ; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
