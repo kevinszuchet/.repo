6eb80cdc114b7286b063e327372ae32327cbcb71
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2020-02-10 15:17:37 -0300

    add kubectl which

--- bashrc.d/kubectl.sh
+++ bashrc.d/kubectl.sh
@@ -1,32 +1,35 @@
-echo 'source <(kubectl completion bash)' >>~/.bashrc
+if which kubectl >/dev/null 2>&1 ; then
+  echo 'source <(kubectl completion bash)' >>~/.bashrc
 
-alias k="kubectl"
-alias kgp="kubectl get pods"
-alias kufront="kubectl config use-context kuberneteca-front"
-alias kujob="kubectl config use-context kuberneteca2"
-alias kufunc="kubectl config use-context kuberneteca-functions-2"
-alias kungfu="kubectl config use-context kuberneteca-functions-2"
-alias kc="kubectl config current-context"
-alias ktop="kufront && k top nodes && kujob && k top nodes && kufunc && k top nodes"
-alias kl="kubectl logs"
+  alias k="kubectl"
+  alias kgp="kubectl get pods"
+  alias kufront="kubectl config use-context kuberneteca-front"
+  alias kujob="kubectl config use-context kuberneteca2"
+  alias kufunc="kubectl config use-context kuberneteca-functions-2"
+  alias kungfu="kubectl config use-context kuberneteca-functions-2"
+  alias kc="kubectl config current-context"
+  alias ktop="kufront && k top nodes && kujob && k top nodes && kufunc && k top nodes"
+  alias kl="kubectl logs"
 
-klw() {
-  if [ -z $1 ] ; then
-    echo "Pod name is needed!" && return 0;
-  fi
-  kl $1 > logs.txt
-}
+  klw() {
+    if [ -z $1 ] ; then
+      echo "Pod name is needed!" && return 0;
+    fi
+    kl $1 > logs.txt
+  }
 
-kfp() {
-  if [ -z $1 ] ; then
-    echo "Pod name is needed!" && return 0;
-  fi
-  kgp --selector=name=$1
-}
+  kfp() {
+    if [ -z $1 ] ; then
+      echo "Pod name is needed!" && return 0;
+    fi
+    kgp --selector=name=$1
+  }
 
-kuconfigmap() {
-  if [ -z $1 ] ; then
-    echo "Configmap name needed!" && return 0;
-  fi
-  kubectl create configmap $1-config --from-env-file=$1.properties
-}
+  kuconfigmap() {
+    if [ -z $1 ] ; then
+      echo "Configmap name needed!" && return 0;
+    fi
+    kubectl create configmap $1-config --from-env-file=$1.properties
+  }
+ fi
+ 
