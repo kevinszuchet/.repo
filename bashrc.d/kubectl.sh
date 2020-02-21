c060b5f89fd19ce09485fbdc8cd1cbdf98e7cba0
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2020-02-21 09:50:20 -0300

    Update kubectl.sh

--- bashrc.d/kubectl.sh
+++ bashrc.d/kubectl.sh
@@ -3,14 +3,24 @@ if which kubectl >/dev/null 2>&1 ; then
 
   alias k="kubectl"
   alias kgp="kubectl get pods"
+  alias ktp="kubectl top pods"
+  alias kgn="kubectl get nodes"
+  alias ktn="kubectl top nodes"
   alias kufront="kubectl config use-context kuberneteca-front"
   alias kufront2="kubectl config use-context kuberneteca-front-2"
-  alias kujob="kubectl config use-context kuberneteca-jobs"
+  alias kujob="kubectl config use-context kuberneteca2"
+  alias kujob2="kubectl config use-context kuberneteca-jobs"
   alias kufunc="kubectl config use-context kuberneteca-functions-2"
   alias kungfu="kubectl config use-context kuberneteca-functions-2"
+  alias kuka="kubectl config use-context kuberneteca-knative"
   alias kc="kubectl config current-context"
   alias ktop="kufront && k top nodes && kujob && k top nodes && kufunc && k top nodes"
   alias kl="kubectl logs"
+  alias kdesc="kubectl describe pod"
+  alias wkgn="watch kubectl get nodes"
+  alias kx="kubectl exec -it"
+  alias wktn="watch kubectl top nodes"
+  alias wktp="watch kubectl top pods"
 
   klw() {
     if [ -z $1 ] ; then
@@ -18,7 +28,16 @@ if which kubectl >/dev/null 2>&1 ; then
     fi
     kl $1 > logs.txt
   }
-
+  
+  wkgp() {
+    if [ "$#" -eq  "1" ]
+    then
+      watch kgp --selector=name=$1
+    else
+      watch kubectl get pods
+    fi
+  }
+  
   kfp() {
     if [ -z $1 ] ; then
       echo "Pod name is needed!" && return 0;
@@ -31,6 +50,18 @@ if which kubectl >/dev/null 2>&1 ; then
       echo "Configmap name needed!" && return 0;
     fi
     kubectl create configmap $1-config --from-env-file=$1.properties
+  }
+  
+  ksh() {
+    kubectl exec -it $1 /bin/sh
+  }
+  
+  kbash() {
+    kubectl exec -it $1 /bin/bash
+  }
+  
+  kimg() {
+    kubectl get deployment "$1-deploy" -o=jsonpath="{$.spec.template.spec.containers[:1].image}"
   }
  fi
  
