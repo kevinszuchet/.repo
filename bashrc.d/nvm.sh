89c521e3dbe2fb42f3f99778830cfb6d86907351
Kevin Szuchet <kevinszuchet@gmail.com>
2020-03-25 09:25:08 -0300

    fix nvm sh

--- bashrc.d/nvm.sh
+++ bashrc.d/nvm.sh
@@ -1,5 +1,5 @@
 # nvm for handling node versions
-if which npm >/dev/null 2>&1 ; then
+if which node >/dev/null 2>&1 ; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
