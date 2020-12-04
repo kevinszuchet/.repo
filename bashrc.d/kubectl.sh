959ed6b8abdbd29bccd9ed500eb88679e161c6f6
Kevin Szuchet <kevinszuchet@gmail.com>
2020-12-04 10:48:18 -0300

    manual merge

--- bashrc.d/kubectl.sh
+++ bashrc.d/kubectl.sh
@@ -8,7 +8,7 @@ if which kubectl >/dev/null 2>&1 ; then
   alias ktn="kubectl top nodes"
   alias kufront="kubectl config use-context kuberneteca-front"
   alias kufront2="kubectl config use-context kuberneteca-front-2"
-  alias kujob="kubectl config use-context kuberneteca2"
+  alias kujob="kubectl config use-context kuberneteca-jobs"
   alias kujob2="kubectl config use-context kuberneteca-jobs"
   alias kufunc="kubectl config use-context kuberneteca-functions-2"
   alias kungfu="kubectl config use-context kuberneteca-functions-2"
@@ -21,7 +21,9 @@ if which kubectl >/dev/null 2>&1 ; then
   alias kx="kubectl exec -it"
   alias wktn="watch kubectl top nodes"
   alias wktp="watch kubectl top pods"
+  alias krep='kubectl get deploy --sort-by "spec.replicas"'
   alias kaws="kubectl config use-context arn:aws:eks:us-east-1:372376101692:cluster/kuberneteca" 
+  alias kdev="kubectl get pods | grep 'Evicted' | awk '{print \$1}' | xargs kubectl delete pod"
   alias kawsf="kubectl config use-context arn:aws:eks:us-east-1:372376101692:cluster/kuberneteca-front"
 
   klw() {
@@ -44,7 +46,7 @@ if which kubectl >/dev/null 2>&1 ; then
     if [ -z $1 ] ; then
       echo "Pod name is needed!" && return 0;
     fi
-    kgp --selector=name=$1
+    kubectl get pods --selector=name=$1
   }
 
   kuconfigmap() {
@@ -53,7 +55,14 @@ if which kubectl >/dev/null 2>&1 ; then
     fi
     kubectl create configmap $1-config --from-env-file=$1.properties
   }
-  
+
+  kecm() {
+    if [ -z $1 ] ; then
+      echo "Configmap name needed!" && return 0;
+    fi
+    kubectl edit configmap $1-config
+  }
+
   kcliconfig() {
     if [ -z $1 ] ; then
       echo "Configmap name needed!" && return 0;
@@ -72,5 +81,42 @@ if which kubectl >/dev/null 2>&1 ; then
   kimg() {
     kubectl get deployment "$1-deploy" -o=jsonpath="{$.spec.template.spec.containers[:1].image}"
   }
+  kfp() {
+	  if [ "$#" -eq  "2" ];
+	  then
+	    kubectl get pods | grep $1 | grep $2
+	  elif [ "$#" -eq  "1" ];
+	  then
+	    kubectl get pods | grep $1
+	  else
+	     kubectl get pods
+	  fi
+	}
+	kcount() {
+	  kubectl get pods | grep $1 | wc -l
+	}
+	kimg() {
+	  kubectl get deployment "$1-deploy" -o=jsonpath="{$.spec.template.spec.containers[:1].image}"
+	}
+	kconf() {
+	  if [ "$#" -eq  "1" ]
+	  then
+	    kubectl get configmap "$1-config" -oyaml
+	  else
+	    kubectl get configmap "$1-config" -oyaml > $2
+	  fi
+	}
+	khpa() {
+	  if [ "$#" -eq  "1" ]
+	  then
+	    kubectl get hpa "$1-deploy" -oyaml
+	  else
+	    kubectl get hpa "$1-deploy" -oyaml > $2
+	  fi
+	}
+
+	appid(){ 
+	  iwannabe $1 --ls | grep PRODUCTECA_CLIENTID
+	}
  fi
  
