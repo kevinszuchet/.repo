3118ca9690bbac2843c1f0500d8c419b109342e6
Kevin Szuchet <kevinszuchet@gmail.com>
2020-12-10 13:18:33 -0300

    kubectl: improvements and new aliases

--- bashrc.d/kubectl.sh
+++ bashrc.d/kubectl.sh
@@ -2,29 +2,36 @@ if which kubectl >/dev/null 2>&1 ; then
   source <(kubectl completion bash)
 
   alias k="kubectl"
-  alias kgp="kubectl get pods"
-  alias ktp="kubectl top pods"
-  alias kgn="kubectl get nodes"
-  alias ktn="kubectl top nodes"
+  # CONTEXT SWITCHES
+  alias kc="kubectl config current-context"
   alias kufront="kubectl config use-context kuberneteca-front"
   alias kufront2="kubectl config use-context kuberneteca-front-2"
   alias kujob="kubectl config use-context kuberneteca-jobs"
   alias kujob2="kubectl config use-context kuberneteca-jobs"
   alias kufunc="kubectl config use-context kuberneteca-functions-2"
   alias kungfu="kubectl config use-context kuberneteca-functions-2"
+  alias kaws="kubectl config use-context arn:aws:eks:us-east-1:372376101692:cluster/kuberneteca" 
+  alias kawsf="kubectl config use-context arn:aws:eks:us-east-1:372376101692:cluster/kuberneteca-front"
   alias kuka="kubectl config use-context kuberneteca-knative"
-  alias kc="kubectl config current-context"
+  # UTILS
+  alias kgp="kubectl get pods"
+  alias ktp="kubectl top pods"
+  alias kgn="kubectl get nodes"
+  alias ktn="kubectl top nodes"
   alias ktop="kufront && k top nodes && kujob && k top nodes && kufunc && k top nodes"
   alias kl="kubectl logs"
   alias kdesc="kubectl describe pod"
   alias wkgn="watch kubectl get nodes"
-  alias kx="kubectl exec -it"
   alias wktn="watch kubectl top nodes"
   alias wktp="watch kubectl top pods"
+  alias kx="kubectl exec -it"
   alias krep='kubectl get deploy --sort-by "spec.replicas"'
-  alias kaws="kubectl config use-context arn:aws:eks:us-east-1:372376101692:cluster/kuberneteca" 
   alias kdev="kubectl get pods | grep 'Evicted' | awk '{print \$1}' | xargs kubectl delete pod"
-  alias kawsf="kubectl config use-context arn:aws:eks:us-east-1:372376101692:cluster/kuberneteca-front"
+  # SPOT and ON DEMAND
+  alias kspot="kubectl get no -l eks.amazonaws.com/capacityType=SPOT"
+  alias kdem="kubectl get no -l producteca/nodegroup-type=ondemand"
+  alias kspotcount="kubectl get no -l eks.amazonaws.com/capacityType=SPOT --no-headers | wc -l"
+  alias kdemcount="kubectl get no -l producteca/nodegroup-type=ondemand --no-headers | wc -l"
 
   klw() {
     if [ -z $1 ] ; then
@@ -69,7 +76,7 @@ if which kubectl >/dev/null 2>&1 ; then
     fi
     kubectl edit hpa $1 
   }
-  
+
   ksh() {
     kubectl exec -it $1 /bin/sh
   }
@@ -81,42 +88,57 @@ if which kubectl >/dev/null 2>&1 ; then
   kimg() {
     kubectl get deployment "$1-deploy" -o=jsonpath="{$.spec.template.spec.containers[:1].image}"
   }
+  
   kfp() {
-	  if [ "$#" -eq  "2" ];
-	  then
-	    kubectl get pods | grep $1 | grep $2
-	  elif [ "$#" -eq  "1" ];
-	  then
-	    kubectl get pods | grep $1
-	  else
-	     kubectl get pods
-	  fi
-	}
-	kcount() {
-	  kubectl get pods | grep $1 | wc -l
-	}
-	kimg() {
-	  kubectl get deployment "$1-deploy" -o=jsonpath="{$.spec.template.spec.containers[:1].image}"
-	}
-	kconf() {
-	  if [ "$#" -eq  "1" ]
-	  then
-	    kubectl get configmap "$1-config" -oyaml
-	  else
-	    kubectl get configmap "$1-config" -oyaml > $2
-	  fi
-	}
-	khpa() {
-	  if [ "$#" -eq  "1" ]
-	  then
-	    kubectl get hpa "$1-deploy" -oyaml
-	  else
-	    kubectl get hpa "$1-deploy" -oyaml > $2
-	  fi
-	}
+    if [ "$#" -eq  "2" ];
+    then
+      kubectl get pods | grep $1 | grep $2
+    elif [ "$#" -eq  "1" ];
+    then
+      kubectl get pods | grep $1
+    else
+      kubectl get pods
+    fi
+  }
+  kcount() {
+    kubectl get pods | grep $1 | wc -l
+  }
+  kimg() {
+    kubectl get deployment "$1-deploy" -o=jsonpath="{$.spec.template.spec.containers[:1].image}"
+  }
+  kconf() {
+    if [ "$#" -eq  "1" ]
+    then
+      kubectl get configmap "$1-config" -oyaml
+    else
+      kubectl get configmap "$1-config" -oyaml > $2
+    fi
+  }
+  khpa() {
+    if [ "$#" -eq  "1" ]
+    then
+      kubectl get hpa "$1-deploy" -oyaml
+    else
+      kubectl get hpa "$1-deploy" -oyaml > $2
+    fi
+  }
 
-	appid(){ 
-	  iwannabe $1 --ls | grep PRODUCTECA_CLIENTID
-	}
- fi
- 
+  appid(){ 
+    iwannabe $1 --ls | grep PRODUCTECA_CLIENTID
+  }
+  kgpn() {
+    kubectl get pods -owide | grep $1
+  }
+  kgpdem() {
+    for A_NODE in $(kubectl get no -l producteca/nodegroup-type=ondemand --no-headers | awk '{print $1}');
+    do
+      kgpn $A_NODE
+    done
+  }
+  kgpspot() {
+    for A_NODE in $(kubectl get no -l eks.amazonaws.com/capacityType=SPOT --no-headers | awk '{print $1}');
+    do
+      kgpn $A_NODE
+    done
+  }
+fi
